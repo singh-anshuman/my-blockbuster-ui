c78a148b4f3067ab107db6295acbdee09d681e9a
singh-anshuman <anshumansingh137@gmail.com>
2019-05-08 01:34:55 +0530

    Added a few code changes and the gitignore file to skip committing the node modules folder.

---
+++ src/components/bbNavigation/bbNavigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class BBNavigation extends React.Component {
+    render() {
+        return (
+            <div>
+                Navigation Bar
+            </div>
+        );
+    }
+}
